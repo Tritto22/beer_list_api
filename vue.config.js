a076f957ed15895a91d7af64aa44b5803f19f31b
Simone <tritto.simone@gmail.com>
2023-12-02 18:48:42 +0100

    public path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/beer_list_api/'  // Sostituisci 'NOME_REPO' con il nome del tuo repository su GitHub
+    : '/'
 })
